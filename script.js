c141628bb957e9e96b2557d75a899ed87f6419fa
Motoki Kasai <kmomoky@gmail.com>
2019-10-30 23:02:04 +0100

    Fine-tune the button hover

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 
 function randomGenerator() {
     let hue = `rgb( ${(Math.floor(Math.random() * 256))}, ${(Math.floor(Math.random() * 256))}, ${(Math.floor(Math.random() * 256))} )`;
-    let randomColorcontainer = document.getElementById('random-color-container').style.background=hue;
+    document.getElementById('random-color-container').style.background=hue;
 }
 
 const colorSwitcher = document.getElementById('random-color');
